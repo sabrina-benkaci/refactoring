72e50e6b418b99199c88a71a2e8e37dd0139b7aa
sabrina-benkaci <56584655+sabrina-benkaci@users.noreply.github.com>
2020-11-15 13:35:53 +0100

    Update TexttestFixture.java

--- src/test/TexttestFixture.java
+++ src/test/TexttestFixture.java
@@ -10,25 +10,24 @@ public class TexttestFixture {
                 new Item("+5 Dexterity Vest", 10, 20),
                 new Item("Aged Brie", 2, 0),
                 new Item("Aged Brie", -8,49),
-	 			new Item("Aged Brie", 16, 80),
-	 			new Item("Aged Brie", -3, 5),
-	 			new Item("Aged Brie", -3, 56),
+	 	new Item("Aged Brie", 16, 80),
+	 	new Item("Aged Brie", -3, 5),
+	 	new Item("Aged Brie", -3, 56),
                 new Item("Elixir of the Mongoose", 5, 7),
                 new Item("Elixir of the Mongoose", -2, 8),
-	 			new Item("Elixir of the Mongoose", -2, -8),
-	 			new Item("Sulfuras, Hand of Ragnaros", 10, 45),
+	 	new Item("Elixir of the Mongoose", -2, -8),
+	 	new Item("Sulfuras, Hand of Ragnaros", 10, 45),
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 6, 48),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 3, 47),
-				new Item("Backstage passes to a TAFKAL80ETC concert", -6, 31),
-				new Item("Backstage passes to a TAFKAL80ETC concert", -6, 50),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 13, 62),
-                // this conjured item does not work properly yet
-				new Item("Conjured Mana Cake", 3, 6) };
+		new Item("Backstage passes to a TAFKAL80ETC concert", 3, 47),
+		new Item("Backstage passes to a TAFKAL80ETC concert", -6, 31),
+		new Item("Backstage passes to a TAFKAL80ETC concert", -6, 50),
+		new Item("Backstage passes to a TAFKAL80ETC concert", 13, 62),
+		new Item("Conjured Mana Cake", 3, 6) };
         GildedRose app = new GildedRose(items);
         int days = 4;
         if (args.length > 0) {
